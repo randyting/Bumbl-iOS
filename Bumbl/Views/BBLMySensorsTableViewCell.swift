c5170d1e35f67688485ccf9dc20efd7f4c69740f
Randy Ting <randy@randyting.com>
2016-01-23 18:37:46 -0800

    Laid out MySensorsTableViewCell.

---
+++ Bumbl/Views/BBLMySensorsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BBLMySensorsTableViewCell.swift
+//  Bumbl
+//
+//  Created by Randy Ting on 1/23/16.
+//  Copyright © 2016 Randy Ting. All rights reserved.
+//
+
+import UIKit
+
+class BBLMySensorsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
