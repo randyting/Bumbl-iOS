673658b1e19819f7c1ad29113b56cff7f0e54d58
Randy Ting <randy@randyting.com>
2016-05-21 17:31:58 -0700

    Updated UI for BBLLoginView. Autolayout warnings for BBLSignInPicker.

---
+++ Bumbl/Extensions/UITextField+Bumbl.swift
@@ -0,0 +1,24 @@
+//
+//  UITextField+Bumbl.swift
+//  Bumbl
+//
+//  Created by Randy Ting on 5/21/16.
+//  Copyright © 2016 Randy Ting. All rights reserved.
+//
+
+import Foundation
+
+extension UITextField {
+  
+  public func insetText(byWidth width: Int){
+    
+    let spacerView = UIView(frame:CGRect(x:0, y:0, width:width, height:10))
+    self.leftViewMode = UITextFieldViewMode.Always
+    self.leftView = spacerView
+    self.rightViewMode = UITextFieldViewMode.Always
+    self.rightView = spacerView
+    
+  }
+  
+  
+}
