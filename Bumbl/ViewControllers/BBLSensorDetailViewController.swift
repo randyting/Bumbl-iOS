e13ed4257352e6092f7606e598fd48ad6cee6a0f
Randy Ting <randy@randyting.com>
2016-08-20 17:55:39 -0700

    Updates default map to point to Apple's new campus.

--- Bumbl/ViewControllers/BBLSensorDetailViewController.swift
+++ Bumbl/ViewControllers/BBLSensorDetailViewController.swift
@@ -64,27 +64,17 @@ class BBLSensorDetailViewController: UIViewController {
   
   private func setupMapView(mapView: MKMapView) {
     // TODO: Grab location from sensor.
-    let location = "1234 Ortega Street, San Francisco, CA 94122"
-    let geocoder = CLGeocoder()
-    geocoder.geocodeAddressString(location) { (placemarks: [CLPlacemark]?, error: NSError?) in
-      if let error = error {
-        print(error.localizedDescription)
-      } else {
-        if let placemarks = placemarks where placemarks.count > 0 {
-         let topResult = placemarks.first!
-          let placemark = MKPlacemark(placemark: topResult)
-          
-          
-          var region = mapView.region
-          region.center = (placemark.region as! CLCircularRegion).center
-          region.span.longitudeDelta /= 1000.0
-          region.span.latitudeDelta /= 1000.0
-          
-          mapView.setRegion(region, animated: true)
-          mapView.addAnnotation(placemark)
-        }
-      }
-    }
+    let placemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: Double(37.3318), longitude: Double(-122.0312)),
+                                addressDictionary: nil)
+    
+    var region = mapView.region
+    region.center = placemark.coordinate
+    region.span.longitudeDelta /= 1000.0
+    region.span.latitudeDelta /= 1000.0
+    
+    mapView.setRegion(region, animated: true)
+    mapView.addAnnotation(placemark)
+    
   }
   
   private func setupAppearanceForSensorValueGaugeView(sensorValueGaugeView: BBLSensorValueGaugeView) {
